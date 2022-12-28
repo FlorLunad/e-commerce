f239f85b4c8e7a9c6d845750ed8a1502af5fc312
Florencia Lunad <florlunadr@gmail.com>
2022-12-27 21:39:18 -0300

    preentrega1

---
+++ src/components/CartWidget.js
@@ -0,0 +1,16 @@
+import { HiShoppingCart } from 'react-icons/hi';
+
+
+const CartWidget = ()=>{
+    
+    const numero = 8;
+    
+    return(
+        <>
+        <HiShoppingCart/>
+        {numero}
+        </>
+    )
+}
+
+export default CartWidget;
